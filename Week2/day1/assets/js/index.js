60faad2b1ffd5a8187f4dc1b004e8e6868dad17f
Gustavo Oliveira <123893190+Gust4voo@users.noreply.github.com>
2023-04-03 16:42:16 +0200

    Add files via upload

---
+++ Week2/day1/assets/js/index.js
@@ -0,0 +1,5 @@
+alert('primo messaggio');
+alert('Sto studiando JS');
+
+document.getElementById("iniziamo la").innerHTML="Iniziamo la nostra pratica"
+document.getElementById("sottotitolo").innerHTML="output scritto da Javascript" 
---
+++ Week2/day1/assets/js/naming.js
@@ -0,0 +1,12 @@
+
+var petpreferito = "toro"
+var pet_preferito = "elefante"
+var petPreferito = 'criceto';
+var PetPreferito4 = 'coniglio';
+
+console.log(petpreferito);
+console.log(pet_preferito);
+console.log(pet_preferito);
+console.log(PetPreferito4);
+
+document.write(pet_preferito + " scritto da Javascript")
