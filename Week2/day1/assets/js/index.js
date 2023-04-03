4afd4d82f049abd4f70b5faaf1761b49a4d61212
Gustavo Oliveira <123893190+Gust4voo@users.noreply.github.com>
2023-04-03 16:42:55 +0200

    Delete Week2/day1 directory

--- Week2/day1/assets/js/index.js
+++
@@ -1,5 +0,0 @@
-alert('primo messaggio');
-alert('Sto studiando JS');
-
-document.getElementById("iniziamo la").innerHTML="Iniziamo la nostra pratica"
-document.getElementById("sottotitolo").innerHTML="output scritto da Javascript" 
